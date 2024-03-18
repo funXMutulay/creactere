fd811eb3f08a4b34742a12fe4162b2104b685a16
funXMutulay <mkebe1@yahoo.fr>
2024-03-18 15:37:24 +0000

    Updates

---
+++ precache-manifest.b43ee83068e914cf21ce10850418e58c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b56cd487d8935ef893331a7ead0d4a6",
+    "url": "/creactere/index.html"
+  },
+  {
+    "revision": "f3d8b72d51883b08eb9c",
+    "url": "/creactere/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/creactere/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/creactere/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "f3d8b72d51883b08eb9c",
+    "url": "/creactere/static/js/main.c672094f.chunk.js"
+  },
+  {
+    "revision": "8e7a8c9303a3daaaf0e7",
+    "url": "/creactere/static/js/runtime~main.1b3a5d6e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/creactere/static/media/logo.06e73328.svg"
+  }
+]);
